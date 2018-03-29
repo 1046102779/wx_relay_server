6ce047aabe9383d5841124301db7303228dcfb27
1046102779 <cdh_cjx@163.com>
2018-03-29 08:49:57 +0800

    m

--- libs/hproses.go
+++ libs/hproses.go
@@ -5,42 +5,15 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/1046102779/common/rpc"
 	"github.com/1046102779/wx_relay_server/conf"
 	. "github.com/1046102779/wx_relay_server/logger"
 )
 
-// 微信开放平台
-type OfficialAccountPlatform struct {
-	Appid                 string `json:"appid"`
-	ComponentVerifyTicket string `json:"component_verify_ticket"`
-	EncodingAesKey        string `json:"encoding_aes_key"`
-	AppSecret             string `json:"app_secret"`
-	ComponentAccessToken  string `json:"component_access_token"`
-	PreAuthCode           string `json:"pre_auth_code"`
-	Token                 string `json:"token"`
-}
-
-// 公众号
-type OfficialAccount struct {
-	Appid                          string `json:"appid"`
-	AuthorizerAccessToken          string `json:"authorizer_access_token"`
-	AuthorizerAccessTokenExpiresIn int64  `json:"authorizer_access_token_expires_in"`
-	AuthorizerRefreshToken         string `json:"authorizer_refresh_token"`
-}
-
-// 授权凭证
-type ComponentVerifyTicket struct {
-	TimeStamp         string `json:"timestamp"`
-	Nonce             string `json:"nonce"`
-	MsgSign           string `json:"msg_sign"`
-	Bts               []byte `json:"bts"`
-	AuthorizationCode string `json:"authorization_code"`
-}
-
 type WxRelayServer struct{}
 
 // 获取公众号平台基本信息
-func (t *WxRelayServer) GetOfficialAccountPlatformInfo() (oap *OfficialAccountPlatform, err error) {
+func (t *WxRelayServer) GetOfficialAccountPlatformInfo() (oap *rpc.OfficialAccountPlatform, err error) {
 	Logger.Info("enter GetOfficialAccountPlatformInfo.")
 	defer Logger.Info("left GetOfficialAccountPlatformInfo.")
 	oap.Appid = conf.WechatAuthTTL.AppId
@@ -54,7 +27,7 @@ func (t *WxRelayServer) GetOfficialAccountPlatformInfo() (oap *OfficialAccountPl
 }
 
 // 存储托管公众号的token相关信息
-func (t *WxRelayServer) StoreOfficialAccountInfo(oa *OfficialAccount) (err error) {
+func (t *WxRelayServer) StoreOfficialAccountInfo(oa *rpc.OfficialAccount) (err error) {
 	Logger.Info("[%v] enter StoreOfficialAccountInfo.", oa.Appid)
 	defer Logger.Info("[%v] left StoreOfficialAccountInfo.", oa.Appid)
 	defer func() { err = nil }()
@@ -79,7 +52,7 @@ func (t *WxRelayServer) StoreOfficialAccountInfo(oa *OfficialAccount) (err error
 }
 
 // 获取公众号token信息
-func (t *WxRelayServer) GetOfficialAccountInfo(appid string) (oa *OfficialAccount, err error) {
+func (t *WxRelayServer) GetOfficialAccountInfo(appid string) (oa *rpc.OfficialAccount, err error) {
 	Logger.Info("[%v] enter GetOfficialAccountInfo.", appid)
 	defer Logger.Info("[%v] left GetOfficialAccountInfo.", appid)
 	defer func() { err = nil }()
@@ -99,7 +72,7 @@ func (t *WxRelayServer) GetOfficialAccountInfo(appid string) (oa *OfficialAccoun
 }
 
 // 刷新component_verify_ticket
-func (t *WxRelayServer) RefreshComponentVerifyTicket(cvt *ComponentVerifyTicket) (code string, err error) {
+func (t *WxRelayServer) RefreshComponentVerifyTicket(cvt *rpc.ComponentVerifyTicket) (code string, err error) {
 	Logger.Info("[%v] enter RefreshComponentVerifyTicket. ", cvt.TimeStamp)
 	defer Logger.Info("[%v] left RefreshComponentVerifyTicket. ", cvt.TimeStamp)
 	defer func() {
